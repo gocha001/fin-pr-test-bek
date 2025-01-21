aab1eecea76cd48c19a02d31629a15d7489a3b2b
gocha001 <kalianichgocha@ukr.net>
2025-01-21 16:32:33 +0200

    minutes

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -21,7 +21,7 @@ export const SMTP = {
     SMTP_FROM: env('SMTP_FROM'),
 };
 
-export const accessTokenLifeTime = 1000 * 60 * 15; // 15 minutes
+export const accessTokenLifeTime = 1000 * 60; // 1 minutes
 // export const accessTokenLifeTime = 1000 * 60 * 60 * 3;
 export const refreshTokenLifeTime = 1000 * 60 * 60 * 24 * 30; // 30 days
 
