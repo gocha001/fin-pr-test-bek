4de23107aff017b8cf452883955791b2aed87aa6
gocha001 <kalianichgocha@ukr.net>
2025-01-21 02:36:22 +0200

    save

--- src/server.js
+++ src/server.js
@@ -1,41 +1,47 @@
 import express from 'express';
-import cors from "cors";
-import pinoHttp from "pino-http";
-import cookieParser from "cookie-parser";
+import cors from 'cors';
+import pinoHttp from 'pino-http';
+import cookieParser from 'cookie-parser';
 
-import router from "./routers/index.js";
+import router from './routers/index.js';
 import { env } from './utils/env.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
-import { UPLOAD_DIR } from "./constants/constants.js";
+import { UPLOAD_DIR } from './constants/constants.js';
 import { swaggerDocs } from './middlewares/swaggerDocs.js';
 const PORT = Number(env('PORT', '3000'));
 
 export const setupServer = () => {
-    const app = express();
+  const app = express();
 
-    app.use(cors());
-    app.use(express.json());
-    app.use(cookieParser());
-    app.use(
-        pinoHttp({
-            transport: {
-                target: 'pino-pretty'
-            }
-        })
-    );
-    app.use('/uploads', express.static(UPLOAD_DIR));
-    app.use('/api-docs', swaggerDocs());
-    app.use(router);
+  app.use(
+    cors({
+      origin: 'https://fin-pr-test-front.vercel.app',
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+      credentials: true,
+    }),
+  );
+  app.use(express.json());
+  app.use(cookieParser());
+  app.use(
+    pinoHttp({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+  app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
+  app.use(router);
 
-    app.use("*", notFoundHandler);
-    app.use(errorHandler);
+  app.use('*', notFoundHandler);
+  app.use(errorHandler);
 
-    return app;
+  return app;
 };
 
 const app = setupServer();
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port: ${PORT}`);
+  console.log(`Server is running on port: ${PORT}`);
 });
